d2923fb40311f14aebae46d48a8b75f10fe978be
xuzongmeng <1102207843@qq.com>
2019-11-12 10:28:21 +0800

    修改

--- query.sql
+++ query.sql
@@ -6,4 +6,6 @@ select rpt.* from hotel h left join hotel_room hr on hr.hotel_id = h.hotel_id le
 
 
 
-select h.* from address a left join address b on a.pr_address_id=b.address_id  left join hotel h on a.address_id = h.address_id where b.name like '%南京%'
+
+
+select h.* from address a left join address b on a.pr_address_id=b.address_id  left join hotel h on a.address_id = h.address_id where b.name like '%南京%' and h.name like '%南京协春%'

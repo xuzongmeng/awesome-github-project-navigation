e2611ab624a26ce680e1f9e92475bbcf9c562459
xuzongmeng <1102207843@qq.com>
2019-11-13 11:32:24 +0800

    修改

--- query.sql
+++ query.sql
@@ -9,3 +9,16 @@ select rpt.* from hotel h left join hotel_room hr on hr.hotel_id = h.hotel_id le
 
 
 select h.* from address a left join address b on a.pr_address_id=b.address_id  left join hotel h on a.address_id = h.address_id where b.name like '%南京%' and h.name like '%南京协春%'
+
+
+
+
+delete from capital_pool_record    where relation_id in (select  a.id from (select o.order_id id from `order` o where user_id = 13304660124237892) a);
+
+delete from account_record    where relation_id in (select  a.id from (select o.order_id id from `order` o where user_id = 13304660124237892) a);
+
+delete from order_room_price   where order_id  in (select  a.id from (select o.order_id id from `order` o where user_id = 13304660124237892) a);
+
+delete from order_customer   where order_id in (select  a.id from (select o.order_id id from `order` o where user_id = 13304660124237892) a);
+
+delete from `order` where user_id= 13304660124237892;
